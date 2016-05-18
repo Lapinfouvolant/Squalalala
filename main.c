9e08f709c705ad26f1958205dd6d918d390eb2a8
Ory Manuel <mory@SA207-01.ensi-bourges.fr>
2016-05-18 11:42:16 +0200

    modif' main à partir du premier rép'

--- main.c
+++ main.c
@@ -6,4 +6,5 @@ int main()
   printf("Hello\n");
   int x= reponse();
   printf("La réponse de l'Univers, de la vie et de tout le reste est %d\n", x);
+  printf("Je modifie main à partir du rep git\n");
 }

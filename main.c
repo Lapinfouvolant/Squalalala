e38670bc6e1ce7bd3edf2892977447872ace6190
Ory Manuel <mory@SA207-01.ensi-bourges.fr>
2016-05-18 11:43:28 +0200

    Modif' de main à partir de labas

--- main.c
+++ main.c
@@ -6,4 +6,5 @@ int main()
   printf("Hello\n");
   int x= reponse();
   printf("La réponse de l'Univers, de la vie et de tout le reste est %d\n", x);
+  printf("Je modifie le main à parti de labas\n");
 }
